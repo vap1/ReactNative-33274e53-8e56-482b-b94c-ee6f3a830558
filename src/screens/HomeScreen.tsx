385108784df1a4107f7d52ca40ca4cc661f73e17
vap1 <vaibhavvasant1705@gmail.com>
2023-08-25 12:12:41 +0530

    First Commit

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,10 +1,13 @@
+Sure! Here's the updated code for the `src/screens/HomeScreen.tsx` file, addressing the comments:
 
+```tsx
 import React, { useEffect, useState } from 'react';
 import { View, Text, StyleSheet } from 'react-native';
-import { APIService } from '../services/APIService';
+import { Lead } from '../types/LeadTypes';
+import { getLeads } from '../apis/LeadApi';
 
 const HomeScreen: React.FC = () => {
-  const [leads, setLeads] = useState([]);
+  const [leads, setLeads] = useState<Lead[]>([]);
 
   useEffect(() => {
     fetchLeads();
@@ -12,8 +15,8 @@ const HomeScreen: React.FC = () => {
 
   const fetchLeads = async () => {
     try {
-      const response = await APIService.getLeads();
-      setLeads(response.data);
+      const response = await getLeads();
+      setLeads(response.leads);
     } catch (error) {
       console.error('Error fetching leads:', error);
     }
@@ -22,10 +25,12 @@ const HomeScreen: React.FC = () => {
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Leads</Text>
-      {leads.map((lead: any) => (
-        <Text key={lead.id} style={styles.leadItem}>
-          {lead.name}
-        </Text>
+      {leads.map((lead: Lead) => (
+        <View key={lead.leadId} style={styles.leadItem}>
+          <Text>{lead.contactDetails}</Text>
+          <Text>{lead.relevantInfo}</Text>
+          {/* Render other lead details */}
+        </View>
       ))}
     </View>
   );
@@ -43,9 +48,11 @@ const styles = StyleSheet.create({
     marginBottom: 16,
   },
   leadItem: {
-    fontSize: 16,
     marginBottom: 8,
   },
 });
 
 export default HomeScreen;
+```
+
+Please note that the code assumes the existence of the `Lead` type in the `src/types/LeadTypes.ts` file and the `getLeads` function in the `src/apis/LeadApi.ts` file. Make sure to update the code accordingly based on your actual implementation.
